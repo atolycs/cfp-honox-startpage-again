ad0bf72a0fffb4ec823bf17caa9fb309d1a48a40
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-12-06 14:04:16 +0900

    feat: init setup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,46 @@
 import { css } from "hono/css";
 import { createRoute } from "honox/factory";
-import Counter from "../islands/counter";
+import Counter from "@/islands/counter";
 
-const className = css`
-  font-family: sans-serif;
-`;
+export const POST = createRoute(async (c) => {
+  console.log();
+  const search_url = "https://duckduckgo.com/";
+  const form_param: string | number | boolean | undefined = (
+    await c.req.formData()
+  )
+    ?.get("search_box")
+    ?.toString();
+
+  const is_url =
+    /^(((http)|(https)):\/\/)?(www\.)?[a-zA-Z0-9]+\.[a-zA-Z]+\/?([a-zA-Z0-9/?=&%-_]+)?$/;
+  const is_ip =
+    /^(((http)|(https)):\/\/)?([0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}|localhost)(:[0-9]{1,5})?(\/[a-zA-Z0-9/?=&%-_]+)?$/;
+
+  // @ts-ignore
+  const url_match: RegExpMatchArray | null = form_param?.match(is_url);
+  // @ts-ignore
+  const ip_match: RegExpMatchArray | null = form_param?.match(is_ip);
+  // biome-ignore format:
+  if (url_match != null) {
+    return c.redirect(
+      url_match[0].substring(0, 4) === "http"
+        ? url_match[0]
+        : `https://${url_match[0]}`,
+    );
+  }
+  if (ip_match != null) {
+    return c.redirect(
+      ip_match[0].substring(0, 4) === "http"
+        ? ip_match[0]
+        : `http://${ip_match[0]}`,
+    );
+  }
+  // @ts-ignore
+  return c.redirect(search_url + encodeURIComponent(form_param));
+  /*   return c.render(
+    <h1> welcome back!</h1>
+  ) */
+});
 
 export default createRoute((c) => {
   const name = c.req.query("name") ?? "Hono";
