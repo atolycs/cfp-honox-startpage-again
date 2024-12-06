589869f610a5b3b3c5527424bd20de9c57d1e660
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-12-06 13:57:16 +0900

    init: setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-build/cloudflare-pages";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
+
+import type { UserConfig } from "vite";
+import "dotenv/config"
+import path from "node:path"
+
+const common: UserConfig = {
+  server: {
+    // @ts-ignore
+    port: Number.parseInt(process.env.DEV_PORT) ?? 5173,
+    host: process.env.HOST ?? "localhost",
+  },
+  build: {
+    rollupOptions: {
+      input: ["./app/server.ts"],
+      output: {
+        assetFileNames: "static/css/[name].[ext]"
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app")
+    }
+  }
+}
 
 export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
-})
+  ...common,
+  plugins: [honox({ devServer: { adapter } }), build()],
+});
